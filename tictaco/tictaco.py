88ef798173032cd15b89ddb8f425ef25ac3dd0d2
Vinayak Mehta <vmehta94@gmail.com>
2020-07-01 04:58:44 +0530

    Add human vs computer game mode

--- tictaco/tictaco.py
+++ tictaco/tictaco.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import random
+
 
 def render_grid(s):
     grid = "\n-----------------\n".join(
@@ -38,7 +40,93 @@ def check_win(s):
     return None
 
 
-def main():
+def get_o_turn(s):
+    _s = list(map(lambda x: x if x.strip() else "", s))
+
+    # check columns
+    for i in [0, 1, 2]:
+        idx_offset = {
+            0: 0,
+            1: 3,
+            2: 6
+        }
+
+        column = [_s[i], _s[i + 3], _s[i + 6]]
+        if column.count("x") == 2 and "" in column:
+            return i + idx_offset[column.index("")] + 1
+
+    # check rows
+    for i in [0, 3, 6]:
+        idx_offset = {
+            0: 0,
+            1: 1,
+            2: 2
+        }
+
+        row = [_s[i], _s[i + 1], _s[i + 2]]
+        if row.count("x") == 2 and "" in row:
+            return i + idx_offset[row.index("")] + 1
+
+    idx_offset = {
+        0: 0,
+        1: 4,
+        2: 8
+    }
+    diagonal1 = [_s[0], _s[4], _s[8]]
+    if diagonal1.count("x") == 2 and "" in diagonal1:
+        return idx_offset[diagonal1.index("")] + 1
+
+    idx_offset = {
+        0: 2,
+        1: 4,
+        2: 6
+    }
+    diagonal2 = [_s[2], _s[4], _s[6]]
+    if diagonal2.count("x") == 2 and "" in diagonal2:
+        return idx_offset[diagonal2.index("")] + 1
+
+    return random.randint(1, 9)
+
+
+def human_vs_computer():
+    s = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    turn = "x"
+    render_grid(s)
+    print(f"{turn} turn")
+
+    while True:
+        if turn == "x":
+            i = int(input())
+            if i not in list(range(1, 10)):
+                print("Invalid input!")
+                continue
+        elif turn == "o":
+            i = get_o_turn(s)
+
+        i -= 1
+        if s[i].strip():
+            print("Slot taken!")
+            continue
+
+        s[i] = turn
+
+        if turn == "x":
+            turn = "o"
+        else:
+            turn = "x"
+
+        print("\033c")
+        render_grid(s)
+
+        player = check_win(s)
+        if player is not None:
+            print(f"{player} wins! 🌮")
+            break
+
+        print(f"{turn} turn")
+
+def human_vs_human():
     s = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
     turn = "x"
@@ -72,3 +160,21 @@ def main():
             break
 
         print(f"{turn} turn")
+
+
+def main():
+    print("\033c")
+    print("Welcome to tictaco! 🌮\n")
+    print("Please select a game mode:\n")
+    print("1. human vs computer")
+    print("2. human vs human\n")
+    game_mode = input("Enter game mode: ")
+
+    if game_mode == "1":
+        print("\033c")
+        human_vs_computer()
+    elif game_mode == "2":
+        print("\033c")
+        human_vs_human()
+    else:
+        raise ValueError("Invalid game mode!")
